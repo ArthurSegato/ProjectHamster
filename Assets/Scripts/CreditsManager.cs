80370cf6883d277c1f6ec8baec6076b0977fb9bf
Arthur Segato Paulo <arthursegato@ctemplar.com>
2020-10-26 15:24:43 -0300

    agora deu certo porra

---
+++ Assets/Scripts/CreditsManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsManager : MonoBehaviour
+{
+    public void AbreArthur()
+    {
+        Application.OpenURL("https://arthursegato.com/");
+    }
+    public void AbreJoao()
+    {
+        Application.OpenURL("https://github.com/JoaoPretti");
+    }
+    public void AbreGustavo()
+    {
+        Application.OpenURL("https://github.com/gustavospelta");
+    }
+    public void AbreMatheus()
+    {
+        Application.OpenURL("https://www.artstation.com/matheusleonor");
+    }
+    public void VoltarMenu()
+    {
+        SceneManager.LoadScene("MenuInicial");
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour
+{
+    public void AbrirCreditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+    public void ExitGame()
+    {
+        //Fecha a aplicação (Funciona apenas no editor)
+        //UnityEditor.EditorApplication.isPlaying = false;
+        //Fecha a aplicação (funciona apenas para o compilado)
+        Application.Quit();
+    }
+}
