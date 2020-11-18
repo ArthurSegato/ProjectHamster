faee7dba777b6143732c5fcbc3d57a1740c6189e
Arthur Segato Paulo <arthursegato@ctemplar.com>
2020-11-17 22:09:14 -0300

    17/11/2020

---
+++ Assets/Scripts/JogadorMovimento.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class JogadorMovimento : MonoBehaviour
+{
+	public float velocidade = 4f;
+	public float alturaPulo = 10f;
+	public bool estaNoChao = false;
+
+	private void Start()
+	{
+		
+	}
+
+	private void Update()
+	{
+		Jump();
+		Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+		transform.position += movement * Time.deltaTime * velocidade;
+	}
+	void Jump()
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, alturaPulo), ForceMode2D.Impulse);
+		}
+	}
+}
